04257fc96f81e3fc61ff884e98bfba338f70379b
Bohdan Pashkovskyi <bpashkovskyi@gmail.com>
2023-09-24 17:02:27 +0300

    Add Scheduler

--- TelegramBots.BotAlarm.Presentation/Program.cs
+++ TelegramBots.BotAlarm.Presentation/Program.cs
@@ -1,34 +1,77 @@
-namespace TelegramBots.BotAlarm.Presentation
+using HealthChecks.UI.Client;
+
+using MediatR;
+
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.EntityFrameworkCore;
+
+using TelegramBots.BotAlarm.Application;
+using TelegramBots.BotAlarm.Domain.Base;
+using TelegramBots.BotAlarm.Infrastructure;
+using TelegramBots.BotAlarm.Infrastructure.Base;
+using TelegramBots.BotAlarm.Persistence;
+using TelegramBots.Shared.Extensions;
+
+namespace TelegramBots.BotAlarm.Presentation;
+
+public static class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Web
+        builder.Services.AddControllers().AddNewtonsoftJson();
+        builder.Services.AddHostedService<Scheduler>();
+
+        // Application
+        builder.Services.AddUpdatesMediator();
+        builder.Services.AddMediatR(typeof(Program));
+        builder.Services.AddScoped<IAlarmService, AlarmService>();
+        builder.Services.AddScoped<IAlarmNotificationService, AlarmNotificationService>();
+        builder.Services.AddScoped<ICurfewService, CurfewService>();
+        builder.Services.AddScoped<ICurfewNotificationService, CurfewNotificationService>();
+        builder.Services.AddScoped<IChatService, ChatService>();
+        builder.Services.AddScoped<IBotService, BotService>();
+
+        // Domain
+        builder.Services.AddScoped<IAlarmStatusCheckingService, AlarmStatusCheckingService>();
+        builder.Services.AddScoped<ICurfewStatusCheckingService, CurfewStatusCheckingService>();
+
+        // Persistence
+        builder.Services.AddDbContext<AlarmBotContext>(options => options.UseSqlServer(
+            builder.Configuration.GetConnectionString("AlarmBotConnection"),
+            sqlServerDbContextOptionsBuilder => sqlServerDbContextOptionsBuilder.MigrationsHistoryTable("__MigrationsHistory", "alarm")));
+
+        // Infrastructure
+        builder.Services.AddScoped<IAlarmApiClient, AlarmApiClient>();
+        builder.Services.AddScoped<ISafeTelegramClient, SafeTelegramClient>();
+
+        builder.Services.AddRollbar(builder.Configuration);
+        builder.Services.AddTelegramClient(builder.Configuration);
+
+        builder.Services.AddHealthChecks(builder.Configuration)
+            .AddDbContextCheck<AlarmBotContext>();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddRazorPages();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapRazorPages();
-
-            app.Run();
+            app.UseDeveloperExceptionPage();
         }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseEndpoints(endpoints => endpoints.MapControllers());
+
+        app.UseHealthChecks(
+            "/healthchecks",
+            new HealthCheckOptions
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,
+            });
+
+        app.Run();
     }
 }
---
+++ TelegramBots.BotAlarm.Presentation/Scheduler.cs
@@ -0,0 +1,54 @@
+﻿using TelegramBots.BotAlarm.Domain.Base;
+
+namespace TelegramBots.BotAlarm.Presentation;
+
+public class Scheduler : IHostedService, IDisposable
+{
+    private readonly IServiceProvider serviceProvider;
+
+    private Timer? timer;
+
+    public Scheduler(IServiceProvider serviceProvider)
+    {
+        this.serviceProvider = serviceProvider;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        this.timer = new Timer(
+            _ =>
+            {
+                var scope = this.serviceProvider.CreateScope();
+                var alarmStatusCheckingService = scope.ServiceProvider.GetRequiredService<IAlarmStatusCheckingService>();
+                alarmStatusCheckingService.CheckForAlarmAndNotifyAsync();
+
+                var scope2 = this.serviceProvider.CreateScope();
+                var curfewStatusCheckingService = scope2.ServiceProvider.GetRequiredService<ICurfewStatusCheckingService>();
+                curfewStatusCheckingService.CheckForCurfewAndNotifyAsync(DateTime.UtcNow);
+            },
+            null,
+            TimeSpan.Zero,
+            TimeSpan.FromSeconds(15));
+
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public void Dispose()
+    {
+        this.Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            this.timer?.Dispose();
+        }
+    }
+}
